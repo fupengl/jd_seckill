1a39eceb648c593da1ea1ef383204af56db81adb
fupeng <fupenglxy@gmail.com>
2020-12-31 13:54:54 +0800

    chore: suport argv

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#! python3
+
 import sys
 from jd_spider_requests import JdSeckill
 
@@ -17,15 +19,23 @@ if __name__ == '__main__':
  1.预约商品
  2.秒杀抢购商品
     """
-    print(a)
-
     jd_seckill = JdSeckill()
-    choice_function = input('请选择:')
-    if choice_function == '1':
-        jd_seckill.reserve()
-    elif choice_function == '2':
-        jd_seckill.seckill_by_proc_pool()
+
+    choice_dict = {
+        "1": jd_seckill.reserve,
+        "2": jd_seckill.seckill_by_proc_pool
+    }
+
+    choice = ''
+
+    if len(sys.argv) == 2:
+        choice = sys.argv[1]
+    else:
+        print(a)
+        choice = input('请选择:')
+
+    if choice in choice_dict:
+        choice_dict[choice]()
     else:
         print('没有此功能')
         sys.exit(1)
-
